72986fe2b06761199d0934374999c156da6970bb
oppong nicholsd <nictech23@gmail.com>
2024-05-27 18:33:43 +0000

    endpoint added

--- backend/src/server.js
+++ backend/src/server.js
@@ -22,7 +22,7 @@ app.use(cookieParser());
 
 app.use((req, res, next) => {
   //res.setHeader('Access-Control-Allow-Origin', [ "http://localhost:3000" || '*' ]);
-  res.setHeader('Access-Control-Allow-Origin', ["https://amalitech-project-client.vercel.app/" || "http://localhost:3000"]);
+  res.setHeader('Access-Control-Allow-Origin', ["https://amalitech-project-client.vercel.app/", "http://localhost:3000"]);
   res.setHeader('Access-Control-Allow-Credentials', 'true');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
