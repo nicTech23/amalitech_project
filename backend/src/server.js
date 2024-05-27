2b86c2fe3abd5822b57e1364f8621029ae77a61f
oppong nicholsd <nictech23@gmail.com>
2024-05-27 18:41:53 +0000

    cors added

--- backend/src/server.js
+++ backend/src/server.js
@@ -7,6 +7,7 @@ const db_connect = require("./config/db_connect")
 const download_route = require("./routes/download_route")
 const message_route = require("./routes/message_route")
 const cookieParser = require("cookie-parser")
+const cors = require("cors")
 
 const app = express()
 
@@ -17,23 +18,24 @@ const port = process.env.PORT || 8000
 //Middleware
 app.use(express.json())
 app.use(cookieParser());
+app.use(cors())
 
 
 
-app.use((req, res, next) => {
-  //res.setHeader('Access-Control-Allow-Origin', [ "http://localhost:3000" || '*' ]);
-  res.setHeader('Access-Control-Allow-Origin', ["https://amalitech-project-client.vercel.app/" || "http://localhost:3000"]);
-  res.setHeader('Access-Control-Allow-Credentials', 'true');
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+// app.use((req, res, next) => {
+//   //res.setHeader('Access-Control-Allow-Origin', [ "http://localhost:3000" || '*' ]);
+//   res.setHeader('Access-Control-Allow-Origin', ["https://amalitech-project-client.vercel.app" || "http://localhost:3000"]);
+//   res.setHeader('Access-Control-Allow-Credentials', 'true');
+//   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+//   res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
   
-  if (req.method === 'OPTIONS') {
-    // Handle preflight requests
-    res.sendStatus(200);
-  } else {
-    next();
-  }
-});
+//   if (req.method === 'OPTIONS') {
+//     // Handle preflight requests
+//     res.sendStatus(200);
+//   } else {
+//     next();
+//   }
+// });
 
 // Serve static files from the 'public' directory
 app.use(express.static('public'));
