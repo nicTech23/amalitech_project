4d181bfbe411f80e5941d0d38f506176489d83e4
oppong nicholsd <nictech23@gmail.com>
2024-05-27 18:49:15 +0000

    cors added

--- backend/src/server.js
+++ backend/src/server.js
@@ -18,13 +18,13 @@ const port = process.env.PORT || 8000
 //Middleware
 app.use(express.json())
 app.use(cookieParser());
-app.use(cors())
+
 
 
 
 app.use((req, res, next) => {
   //res.setHeader('Access-Control-Allow-Origin', [ "http://localhost:3000" || '*' ]);
-  res.setHeader('Access-Control-Allow-Origin', "https://amalitech-project-client.vercel.app");
+  res.setHeader('Access-Control-Allow-Origin', "https://amalitech-project-client.vercel.app/" );
   res.setHeader('Access-Control-Allow-Credentials', 'true');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
